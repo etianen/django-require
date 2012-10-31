20ffd1a3b24ba342bde997684bc4f23455c2dca2
Dave Hall <dave@etianen.com>
2012-10-31 18:00:44 +0000

    require_module templatetag now supports module names relative to REQUIRE_BASE_URL and standalone modules.

--- require/templatetags/require.py
+++ require/templatetags/require.py
@@ -4,16 +4,20 @@ from django import template
 
 from django.contrib.staticfiles.storage import staticfiles_storage
 
-from require.settings import REQUIRE_JS
-from require.helpers import resolve_require_url
+from require.settings import REQUIRE_JS, REQUIRE_STANDALONE_MODULES, REQUIRE_DEBUG
+from require.helpers import resolve_require_url, resolve_require_module
 
 
 register = template.Library()
 
 
 @register.simple_tag
-def require_module(data_main):
-    return u"""<script src="{src}" data-main="{data_main}"></script>""".format(
+def require_module(module):
+    if not REQUIRE_DEBUG and module in REQUIRE_STANDALONE_MODULES:
+        return u"""<script src="{module}">""".format(
+            module = staticfiles_storage.url(resolve_require_module(module)),
+        )
+    return u"""<script src="{src}" data-main="{module}"></script>""".format(
         src = staticfiles_storage.url(resolve_require_url(REQUIRE_JS)),
-        data_main = staticfiles_storage.url(data_main),
+        module = staticfiles_storage.url(resolve_require_module(module)),
     )
