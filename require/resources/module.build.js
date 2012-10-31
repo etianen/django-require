a19161ee8f364323cee1a3d2b5a47f74d7f5b401
Dave Hall <dave@etianen.com>
2012-10-31 18:01:44 +0000

    Adding in module build script and almond.js for standalone compiles.

---
+++ require/resources/module.build.js
@@ -0,0 +1,15 @@
+/**
+ * Build profile for a standalone django-require module, packaged with almond.js.
+ * 
+ * This supports all the normal configuration available to a r.js build profile. The only gotchas are:
+ *
+ *   - 'baseUrl' will be overidden by django-require during the build process.
+ *   - 'name' will be overidden by django-require during the build process.
+ *   - 'include' will be overidden by django-require during the build process.
+ *   - 'out' will be overidden by django-require during the build process. 
+ */
+({
+    optimize: "closure",
+    preserveLicenseComments: false,
+    wrap: true
+})
