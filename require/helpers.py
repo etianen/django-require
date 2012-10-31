b1b7299f92c410051dbba39ac9655fb9dc75e276
Dave Hall <dave@etianen.com>
2012-10-31 18:00:11 +0000

    Adding in settings for standalone moduile compilation.

--- require/helpers.py
+++ require/helpers.py
@@ -5,3 +5,9 @@ from require.settings import REQUIRE_BASE_URL
 
 def resolve_require_url(name):
     return posixpath.normpath(posixpath.join(REQUIRE_BASE_URL, name))
+
+
+def resolve_require_module(name):
+    if not posixpath.splitext(name)[-1].lower() == ".js":
+        name += ".js"
+    return resolve_require_url(name)
--- require/settings.py
+++ require/settings.py
@@ -6,3 +6,7 @@ REQUIRE_BASE_URL = getattr(settings, "REQUIRE_BASE_URL", "js")
 REQUIRE_BUILD_PROFILE = getattr(settings, "REQUIRE_BUILD_PROFILE", "app.build.js")
 
 REQUIRE_JS = getattr(settings, "REQUIRE_JS", "require.js")
+
+REQUIRE_STANDALONE_MODULES = getattr(settings, "REQUIRE_STANDALONE_MODULES", ()) 
+
+REQUIRE_DEBUG = getattr(settings, "REQUIRE_DEBUG", settings.DEBUG)
