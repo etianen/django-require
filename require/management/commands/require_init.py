3fd6b97f4f4f2593b74372f418c148453971695a
Dave Hall <dave@etianen.com>
2012-11-01 15:06:00 +0000

    Adding in destination dir option to require_init.

--- require/management/commands/require_init.py
+++ require/management/commands/require_init.py
@@ -1,15 +1,26 @@
 import os.path, shutil
 from optparse import make_option
 
-from django.core.management.base import NoArgsCommand, CommandError
+from django.core.management.base import NoArgsCommand
 from django.conf import settings
 
 from require.settings import REQUIRE_BASE_URL, REQUIRE_BUILD_PROFILE, REQUIRE_JS, REQUIRE_STANDALONE_MODULES
 
 
+def default_staticfiles_dir():
+    staticfiles_dirs = getattr(settings, "STATICFILES_DIRS", ())
+    if len(staticfiles_dirs) == 0:
+        return None
+    return staticfiles_dirs[0]
+
+
 class Command(NoArgsCommand):
     
-    help = "Copy the base require.js files into your STATICFILES_DIRS."
+    help = (
+        "Copies the base require.js file into your STATICFILES_DIRS.\n\n"
+        "Also copies default implementations of any build profiles listed in the REQUIRE_BUILD_PROFILE "
+        "and REQUIRE_STANDALONE_MODULES settings."
+    )
     
     option_list = NoArgsCommand.option_list + (
         make_option(
@@ -20,16 +31,21 @@ class Command(NoArgsCommand):
             default = False,
             help = "Overwrite existing files if found.", 
         ),
+        make_option(
+            "-d",
+            "--dir",
+            action = "store",
+            dest = "dir",
+            default = default_staticfiles_dir(),
+            help = "Copy files into the named directory. Defaults to the first item in your STATICFILES_DIRS setting.", 
+        ),
     )
     
     requires_model_validation = False
     
     def handle_noargs(self, **options):
-        # Get the destination dir.
-        staticfiles_dirs = getattr(settings, "STATICFILES_DIRS", ())
-        if len(staticfiles_dirs) != 1:
-            raise CommandError("Expected settings.STATICFILES_DIRS to contain one item, aborting.")
-        dst_dir = staticfiles_dirs[0]
+        verbosity = int(options.get("verbosity", 1))
+        dst_dir = options["dir"]
         # Calculate paths.
         resources_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "..", "resources"))
         resources = [
@@ -44,7 +60,12 @@ class Command(NoArgsCommand):
         for resource_name, dst_name in resources:
             dst_path = os.path.abspath(os.path.join(dst_dir, REQUIRE_BASE_URL, dst_name))
             if os.path.exists(dst_path) and not options["force"]:
-                self.stdout.write("{} already exists, skipping.\n".format(dst_path))
+                if verbosity > 0:
+                    self.stdout.write("{} already exists, skipping.\n".format(dst_path))
             else:
+                dst_dirname = os.path.dirname(dst_path)
+                if not os.path.exists(dst_dirname):
+                    os.makedirs(dst_dirname)
                 shutil.copyfile(os.path.join(resources_dir, resource_name), dst_path)
-                self.stdout.write("Copied {} to {}.\n".format(resource_name, dst_path))
+                if verbosity > 0:
+                    self.stdout.write("Copied {} to {}.\n".format(resource_name, dst_path))
