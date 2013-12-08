bd2e94f4e5e0b6b7c79fa9c9c595453263398af2
Dave Hall <dave@etianen.com>
2013-12-08 14:46:21 +0000

    Advertising Python 2.6 support

--- setup.py
+++ setup.py
@@ -37,6 +37,7 @@ setup(
         "Programming Language :: Python",
         "Programming Language :: Python",
         "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.6",
         "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.2",
