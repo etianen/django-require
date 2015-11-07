52ee62ddd628930849ac4a3b6e91a6e181d49b04
László Károlyi <laszlo@karolyi.hu>
2015-11-07 16:23:22 +0100

    Adding python versions

--- setup.py
+++ setup.py
@@ -3,44 +3,47 @@ from distutils.core import setup
 from require import __version__
 
 
-version_str = ".".join(str(n) for n in __version__)
+version_str = '.'.join(str(n) for n in __version__)
 
 
 setup(
-    name = "django-require",
-    version = version_str,
-    license = "BSD",
-    description = "A Django staticfiles post-processor for optimizing with RequireJS.",
-    author = "Dave Hall",
-    author_email = "dave@etianen.com",
-    url = "https://github.com/etianen/django-require",
-    packages = [
-        "require",
-        "require.management",
-        "require.management.commands",
-        "require.templatetags",
+    name='django-require',
+    version=version_str,
+    license='BSD',
+    description=(
+        'A Django staticfiles post-processor for optimizing with RequireJS.'),
+    author='Dave Hall',
+    author_email='dave@etianen.com',
+    url='https://github.com/etianen/django-require',
+    packages=[
+        'require',
+        'require.management',
+        'require.management.commands',
+        'require.templatetags',
     ],
-    package_data = {
-        "require": [
-            "resources/*.jar",
-            "resources/*.js",
-            "resources/tests/*.js",
+    package_data={
+        'require': [
+            'resources/*.jar',
+            'resources/*.js',
+            'resources/tests/*.js',
         ],
     },
-    classifiers = [
-        "Development Status :: 5 - Production/Stable",
-        "Environment :: Web Environment",
-        "Framework :: Django",
-        "Intended Audience :: Developers",
-        "License :: OSI Approved :: BSD License",
-        "Operating System :: OS Independent",
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 2",
-        "Programming Language :: Python :: 2.6",
-        "Programming Language :: Python :: 2.7",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.2",
-        "Programming Language :: Python :: 3.3",
-        "Topic :: Internet :: WWW/HTTP",
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Topic :: Internet :: WWW/HTTP',
     ],
 )
